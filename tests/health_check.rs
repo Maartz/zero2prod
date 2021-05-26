5a14bbf83fe189c71a2e94abc85918ecb6bdd9db
William <w.martin38@icloud.com>
2021-05-26 16:35:34 +0200

    add health_check feature

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,12 +1,13 @@
-use zero2prod::main;
+use std::net::TcpListener;
+use tokio::spawn;
 
 #[actix_rt::test]
 async fn health_check_works() {
-    spwan_app().await.expect("Failed to spawn our app");
-
+    let address = spawn_app();
     let client = reqwest::Client::new();
 
-    let response = client.get("http://127.0.0.1:7777/health_check")
+    let response = client
+        .get(&format!("{}/health_check", &address))
         .send()
         .await
         .expect("Failed to execute request.");
@@ -15,6 +16,10 @@ async fn health_check_works() {
     assert_eq!(Some(0), response.content_length());
 }
 
-async fn spwan_app() -> std::io::Result<()> {
-    todo!()
+fn spawn_app() -> String {
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+    let port = listener.local_addr().unwrap().port();
+    let server = zero2prod::run(listener).expect("Failed to bind address");
+    let _ = tokio::spawn(server);
+    format!("http://127.0.0.1:{}", port)
 }
